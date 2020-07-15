f2643fdecf44faa7e7c756db62378b99ed3b053a
AbdulMalik <abdumaliksharipov@gmail.com>
2020-07-15 21:46:27 +0500

    Init1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+
+
+import React, { Component } from 'react';
+import axios from "axios";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component {
+
+    state= {advice:""};
+
+    componentDidMount(){
+        this.fetchAdvice();        
+    }
+
+    fetchAdvice = ()=>{
+        axios.get('https://api.adviceslip.com/advice')
+        .then((res)=>{
+            const {advice}= res.data.slip;
+            this.setState({advice})            
+
+        })
+        .catch((err)=>{
+            console.log(err);            
+
+        })
+    }
+
+
+    render() {
+        const {advice}=this.state;
+        return (
+            <div className="app">
+                <div className="card">
+                    <h2 className="heading">{advice}</h2>
+                </div>
+            </div>
+           
+        )
+    }
 }
 
 export default App;
